acec894fb601b6b484d12f825beb11407a9268de
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-10-29 21:40:21 -0600

    commit

---
+++ Nunit_Test1/Nunit_Test1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nunit_Test1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nunit_Test1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44e89e38-912f-4a39-a908-fb00531db5ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Nunit_Test1/Nunit_Test1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Nunit_Test1
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Url = "https://www.google.com/";            
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            driver.Url = "https://www.google.com/";
+        }
+    }
+}
