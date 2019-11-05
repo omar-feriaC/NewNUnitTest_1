912efb00c6d000f446008e4af500413302cffc85
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-11-05 10:09:09 -0600

    commit

--- Nunit_Test1/Nunit_Test1/UnitTest1.cs
+++ Nunit_Test1/Nunit_Test1/UnitTest1.cs
@@ -5,6 +5,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
+using System.Net;
+using System.IO;
 
 namespace Nunit_Test1
 {
@@ -28,7 +30,44 @@ namespace Nunit_Test1
         [Test]
         public void TestMethod1()
         {
-            driver.Url = "https://www.google.com/";            
+            driver.Url = "https://www.google.com/";
+            // Create a request using a URL that can receive a post. 
+
+            /*WebRequest request = WebRequest.Create("https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.
+            string postData = "80001";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();*/
+
+
+
         }
 
         [Test]
